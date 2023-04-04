d964c9a81a1a2aa263ddb4784165d671deb2ddea
Alula65 <alulasema@gmail.com>
2023-04-03 18:39:33 -0700

    0x07

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -7,22 +7,20 @@
  */
 unsigned int _strspn(char *s, char *accept)
 {
-	unsigned int n = 0;
-	int r;
+	int z = 0, x, y;
 
-	while (*s)
+	for (x = 0; s[x] != '\0'; x++)
 	{
-		for (r = 0; accept[r]; r++)
+		if (s[x] != 32)
 		{
-			if (*s == accept[r])
+			for (y = 0; accept[y] != '\0'; y++)
 			{
-				n++;
-				break;
+				if (s[x] == accept[y])
+					z++;
 			}
-			else if (accept[r + 1] == '\0')
-				return (n);
 		}
-		s++;
+		else
+			return (z);
 	}
-	return (n);
+	return (z);
 }
