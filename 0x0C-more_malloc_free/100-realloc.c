e0f7ec82d9f759ced277995b1bd5a3e1765ea4e6
Alula65 <alulasema@gmail.com>
2023-04-12 14:51:32 -0700

    0x0C-more_malloc_free

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -1,41 +1,50 @@
-#include "main.h"
 #include <stdlib.h>
-#include <stdio.h>
-
-#define ERR_MSG "Error"
+#include "main.h"
 
 /**
- * is_digit - checks if a string contains a non-digit char
- * @s: string to be evaluated
+ * *_realloc - reallocates a memory block using malloc and free
+ * @ptr: pointer to the memory previsouly allocated by malloc
+ * @old_size: size of the allocated memory for ptr
+ * @new_size: new size of the new memory block
  *
- * Return: 0 if a non-digit is found, 1 otherwise
+ * Return: pointer to the newly allocated memory block
  */
-int is_digit(char *s)
+void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 {
-	int i = 0;
+	char *ptr1;
+	char *old_ptr;
+	unsigned int i;
 
-	while (s[i])
+	if (new_size == old_size)
+		return (ptr);
+
+	if (new_size == 0 && ptr)
 	{
-		if (s[i] < '0' || s[i] > '9')
-			return (0);
-		i++;
+		free(ptr);
+		return (NULL);
 	}
-	return (1);
-}
 
-/**
- * _strlen - returns the length of a string
- * @s: string to evaluate
- *
- * Return: the length of the string
- */
-int _strlen(char *s)
-{
-	int i = 0;
+	if (!ptr)
+		return (malloc(new_size));
 
-	while (s[i] != '\0')
+	ptr1 = malloc(new_size);
+	if (!ptr1)
+		return (NULL);
+
+	old_ptr = ptr;
+
+	if (new_size < old_size)
 	{
-		i++;
+		for (i = 0; i < new_size; i++)
+			ptr1[i] = old_ptr[i];
 	}
-	return (i);
+
+	if (new_size > old_size)
+	{
+		for (i = 0; i < old_size; i++)
+			ptr1[i] = old_ptr[i];
+	}
+
+	free(ptr);
+	return (ptr1);
 }
